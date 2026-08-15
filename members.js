a2862ceec0bd9d2474f65b6e0ce6f41a224810ef
dimasenchilo2017-commits <narlox81@gmail.com>
2026-08-15 23:27:13 +0300

    заменил members.js на новый

--- members.js
+++ members.js
@@ -2,20 +2,20 @@
 module.exports = [
     // ===== ЛИДЕР =====
     { discordId: '379679942910410762', gameName: 'Artem Nuerra', group: 'lead' },
-    { discordId: '379679942910410762', gameName: 'Yuto Nuerra', group: 'lead' },
+    { discordId: '702879971252895774', gameName: 'Yuto Nuerra', group: 'lead' },
     
     // ===== ЗАМЕСТИТЕЛИ ЛИДЕРА =====
-    { discordId: '278475047562117131', gameName: 'Karterra Nuerra', group: 'dep_lead' },
-    { discordId: '1306310033369202779', gameName: 'Yuki Nuerra', group: 'dep_lead' },
+    { discordId: '1249797110761525432', gameName: 'Karterra Nuerra', group: 'dep_lead' },
+    { discordId: '813271211358027777', gameName: 'Yuki Nuerra', group: 'dep_lead' },
     
     // ===== H.NUERRA (высшее руководство) =====
-    { discordId: '702529657718833162', gameName: 'Manera Nuerra', group: 'h_nuerra' },
-    { discordId: '702879971252895774', gameName: 'Kyle Nuerra', group: 'h_nuerra' },
-    { discordId: '558188713134522368', gameName: 'Cursa Nuerra', group: 'h_nuerra' },
-    { discordId: '553870010750205952', gameName: 'John Nuerra', group: 'h_nuerra' },
+    { discordId: '1132347777314193651', gameName: 'Manera Nuerra', group: 'h_nuerra' },
+    { discordId: '553870010750205952', gameName: 'Kyle Nuerra', group: 'h_nuerra' },
+    { discordId: '562921505219018793', gameName: 'Cursa Nuerra', group: 'h_nuerra' },
+    { discordId: '753619983271198881', gameName: 'John Nuerra', group: 'h_nuerra' },
     
     // ===== NUERRA (основной состав) =====
-    { discordId: '1132347777314193651', gameName: '', group: 'nuerra' },
-    { discordId: '230437735347781633', gameName: '', group: 'nuerra' },
-    { discordId: '562921505219018793', gameName: '', group: 'nuerra' },
+//  { discordId: '', gameName: '', group: 'nuerra' },
+//    { discordId: '', gameName: '', group: 'nuerra' },
+//    { discordId: '', gameName: '', group: 'nuerra' },
 ];
